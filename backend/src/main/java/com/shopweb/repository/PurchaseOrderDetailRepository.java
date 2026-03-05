d197fc730c1afd414b777253ddb8f9d2556897cf
duyanhng1 <anhndhe171592@fpt.edu.vn>
2026-03-05 22:29:58 +0700

    update code

--- backend/src/main/java/com/shopweb/repository/PurchaseOrderDetailRepository.java
+++
@@ -1,9 +0,0 @@
-package com.shopweb.repository;
-
-import com.shopweb.model.entity.PurchaseOrderDetail;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PurchaseOrderDetailRepository extends JpaRepository<PurchaseOrderDetail, Long> {
-}
--- backend/src/main/java/com/shopweb/repository/PurchaseOrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.shopweb.repository;
-
-import com.shopweb.model.entity.PurchaseOrder;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PurchaseOrderRepository extends JpaRepository<PurchaseOrder, Long> {
-}
